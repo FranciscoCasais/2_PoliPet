fff5605aade4ed3056e989df00f06229c991f177
Posado-Sebastian <101263239+Posado-Sebastian@users.noreply.github.com>
2023-06-06 09:14:39 -0300

    DER y DB

--- PoliPetDB.sql
+++ PoliPetDB.sql
@@ -1,23 +1,8 @@
--- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
--- -----------------------------------------------------
--- Table `mydb`.`solicitudes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`solicitudes` (
+CREATE TABLE IF NOT EXISTS `mydb`.`solicitud` (
   `idSolicitud` INT NOT NULL,
   `estado` VARCHAR(45) NULL,
   `fechaAdopcion` DATE NULL,
@@ -25,11 +10,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`solicitudes` (
   PRIMARY KEY (`idSolicitud`))
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`animales`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`animales` (
+CREATE TABLE IF NOT EXISTS `mydb`.`animal` (
   `idAnimal` INT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
   `especie` VARCHAR(45) NULL,
@@ -42,16 +23,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`animales` (
   INDEX `fk_animales_solicitudes1_idx` (`solicitudes_idSolicitud` ASC) VISIBLE,
   CONSTRAINT `fk_animales_solicitudes1`
     FOREIGN KEY (`solicitudes_idSolicitud`)
-    REFERENCES `mydb`.`solicitudes` (`idSolicitud`)
+    REFERENCES `mydb`.`solicitud` (`idSolicitud`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`personas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`personas` (
+CREATE TABLE IF NOT EXISTS `mydb`.`persona` (
   `idPersona` INT NOT NULL,
   `nombre` VARCHAR(45) NULL,
   `email` VARCHAR(45) NULL,
@@ -64,16 +41,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`personas` (
   INDEX `fk_personas_solicitudes1_idx` (`solicitudes_idSolicitud` ASC) VISIBLE,
   CONSTRAINT `fk_personas_solicitudes1`
     FOREIGN KEY (`solicitudes_idSolicitud`)
-    REFERENCES `mydb`.`solicitudes` (`idSolicitud`)
+    REFERENCES `mydb`.`solicitud` (`idSolicitud`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`vacunas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`vacunas` (
+CREATE TABLE IF NOT EXISTS `mydb`.`vacuna` (
   `idVacuna` INT NOT NULL,
   `dosis` INT NULL,
   `fecha` DATE NULL,
@@ -82,32 +55,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`vacunas` (
   INDEX `fk_vacunas_animales1_idx` (`animales_idAnimal` ASC) VISIBLE,
   CONSTRAINT `fk_vacunas_animales1`
     FOREIGN KEY (`animales_idAnimal`)
-    REFERENCES `mydb`.`animales` (`idAnimal`)
+    REFERENCES `mydb`.`animal` (`idAnimal`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`solicitudes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`solicitudes` (
-  `idSolicitud` INT NOT NULL,
-  `estado` VARCHAR(45) NULL,
-  `fechaAdopcion` DATE NULL,
-  `fechaEnvio` DATE NULL,
-  PRIMARY KEY (`idSolicitud`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`table5`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`table5` (
-)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
