475f74158b8bed7e408227ff2ba4c0c647ca3cb4
Francisco <franjcasis@gmail.com>
2023-06-16 17:10:25 -0300

    Última versión

--- UltimaVersion/polipet.sql
+++ UltimaVersion/polipet.sql
@@ -5,13 +5,17 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema PoliPet
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema PoliPet
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `PoliPet` ;
+drop schema if exists `PoliPet`;
+CREATE SCHEMA IF NOT EXISTS `PoliPet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `PoliPet` ;
 
 -- -----------------------------------------------------
@@ -19,14 +23,16 @@ USE `PoliPet` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `PoliPet`.`Animal` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
-  `Especie` VARCHAR(45) NULL,
-  `Raza` VARCHAR(45) NULL,
-  `Fecha_nacimiento` DATE NULL,
-  `Genero` VARCHAR(45) NULL,
-  `Descripcion` VARCHAR(400) NULL,
+  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `Especie` VARCHAR(45) NULL DEFAULT NULL,
+  `Raza` VARCHAR(45) NULL DEFAULT NULL,
+  `Fecha_nacimiento` DATE NULL DEFAULT NULL,
+  `Genero` VARCHAR(45) NULL DEFAULT NULL,
+  `Descripcion` VARCHAR(400) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -34,9 +40,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `PoliPet`.`Vacuna` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
+  `Nombre` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -45,38 +53,20 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `PoliPet`.`DetallesVacuna` (
   `Animal_ID` INT NOT NULL,
   `Vacuna_ID` INT NOT NULL,
-  `Dosis` FLOAT NULL,
-  `Fecha` DATE NULL,
+  `Dosis` FLOAT NULL DEFAULT NULL,
+  `Fecha` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`Animal_ID`, `Vacuna_ID`),
   INDEX `fk_Animal_has_Vacuna_Vacuna1_idx` (`Vacuna_ID` ASC) VISIBLE,
   INDEX `fk_Animal_has_Vacuna_Animal_idx` (`Animal_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Animal_has_Vacuna_Animal`
     FOREIGN KEY (`Animal_ID`)
-    REFERENCES `PoliPet`.`Animal` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `PoliPet`.`Animal` (`ID`),
   CONSTRAINT `fk_Animal_has_Vacuna_Vacuna1`
     FOREIGN KEY (`Vacuna_ID`)
-    REFERENCES `PoliPet`.`Vacuna` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `PoliPet`.`Persona`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `PoliPet`.`Persona` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `Nombre_completo` VARCHAR(45) NULL,
-  `Email` VARCHAR(45) NULL,
-  `Telefono` VARCHAR(45) NULL,
-  `Direccion` VARCHAR(45) NULL,
-  `Fecha_nacimiento` DATE NULL,
-  `Ocupacion` VARCHAR(45) NULL,
-  `Experiencia_previa` TINYINT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+    REFERENCES `PoliPet`.`Vacuna` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -84,9 +74,29 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `PoliPet`.`Estado` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Detalles` VARCHAR(45) NULL,
+  `Detalles` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `PoliPet`.`Persona`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PoliPet`.`Persona` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Nombre_completo` VARCHAR(45) NULL DEFAULT NULL,
+  `Email` VARCHAR(45) NULL DEFAULT NULL,
+  `Telefono` VARCHAR(45) NULL DEFAULT NULL,
+  `Direccion` VARCHAR(45) NULL DEFAULT NULL,
+  `Fecha_nacimiento` DATE NULL DEFAULT NULL,
+  `Ocupacion` VARCHAR(45) NULL DEFAULT NULL,
+  `Experiencia_previa` TINYINT NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -95,28 +105,25 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `PoliPet`.`Solicitud` (
   `Persona_ID` INT NOT NULL,
   `Animal_ID` INT NOT NULL,
-  `Fecha_envio` DATE NULL,
+  `Fecha_envio` DATE NULL DEFAULT NULL,
+  `Fecha_adopcion` DATE NULL,
   `Estado_ID` INT NOT NULL,
   PRIMARY KEY (`Persona_ID`, `Animal_ID`),
   INDEX `fk_Persona_has_Animal_Animal1_idx` (`Animal_ID` ASC) VISIBLE,
   INDEX `fk_Persona_has_Animal_Persona1_idx` (`Persona_ID` ASC) VISIBLE,
   INDEX `fk_Solicitud_Estado1_idx` (`Estado_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Persona_has_Animal_Persona1`
-    FOREIGN KEY (`Persona_ID`)
-    REFERENCES `PoliPet`.`Persona` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Persona_has_Animal_Animal1`
     FOREIGN KEY (`Animal_ID`)
-    REFERENCES `PoliPet`.`Animal` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `PoliPet`.`Animal` (`ID`),
+  CONSTRAINT `fk_Persona_has_Animal_Persona1`
+    FOREIGN KEY (`Persona_ID`)
+    REFERENCES `PoliPet`.`Persona` (`ID`),
   CONSTRAINT `fk_Solicitud_Estado1`
     FOREIGN KEY (`Estado_ID`)
-    REFERENCES `PoliPet`.`Estado` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `PoliPet`.`Estado` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
