78566d56c669b3285f41482ced2de46022a3481f
FranciscoCasais <101185430+FranciscoCasais@users.noreply.github.com>
2023-06-23 14:52:45 -0300

    Delete PoliPetJavaCasaisPerlo directory

--- PoliPetJavaCasaisPerlo/poliPet_Java/src/Estado.java
+++
@@ -1,7 +0,0 @@
-public enum Estado {
-    PENDIENTE("Pendiente"),APROBADO("Aprobado"),RECHAZADO("Rechazado");
-    private String estado;
-    private Estado(String estado) { this.estado=estado; }
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
-}
