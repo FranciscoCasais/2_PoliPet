b73633740287df73d9c6f960c53d862f49c3d952
Posado-Sebastian <101263239+Posado-Sebastian@users.noreply.github.com>
2023-06-28 11:15:52 -0300

    Main que muestra todos los metodos hecho

--- PoliPet/src/Main.java
+++ PoliPet/src/Main.java
@@ -19,19 +19,66 @@ public class Main {
         }
 
         Sistema sist=new Sistema();
+
         sist.setAnimales(bdd.obtenerValoresAnimal());
         sist.setPersonas(bdd.obtenerValoresPersona());
         sist.setSolicitudes(bdd.obtenerValoresSolicitud(sist.getAnimales(), sist.getPersonas()));
-        /*for(Animal a:sist.getAnimales()){
-            System.out.println(a.toString());
+        Animal animal = sist.animalMenorEdad();
+        HashSet<Solicitud> solicituds = sist.solicitudesNoValidas();
+
+        System.out.println("\nEjercicio A: Lista de todos los animales presentes en una solicitud de adopcion");
+        sist.animalesSolicitud();
+
+        System.out.println("\nEjercicio B: El animal de menor edad disponible para adopcion");
+        System.out.println(animal.toString());
+
+        System.out.println("\nEjercicio C: Lista de solicitudes no validas");
+        if(solicituds.size()==0){
+            System.out.println("No hay valores");
         }
-        for(Persona p: sist.getPersonas()){
-            System.out.println(p.toString());
-        }*/
-        HashSet<Solicitud> sol=sist.solicitudesNoValidas();
-        for(Solicitud s:sol){
-            System.out.println(s.toString());
+        else {
+            for (Solicitud s : solicituds) {
+                System.out.println(s.toString());
+            }
         }
 
+        System.out.println("\nEjercicio D: Corrige las fechas mal cargadas");
+        sist.corregirFechas();
+
+        System.out.println("\nEjercicio E: Muestra las personas que rompen la regla de adoptar una vez al mes");
+        HashSet<Persona> rompReg=sist.rompeRegla();
+        if(rompReg.size()==0){
+            System.out.println("No hay valores");
+        }
+        else {
+            for (Persona p : rompReg) {
+                System.out.println(p.toString());
+            }
+        }
+
+        System.out.println("\nEjercicio F: Muestra las personas con experiencia en el cuidado de mascotas por solicitud de adopción");
+        HashSet<Persona> experimentads=sist.personasExperimentadas();
+        if(experimentads.size()==0){
+            System.out.println("No hay valores");
+        }
+        else {
+            for (Persona p : experimentads) {
+                System.out.println(p.toString());
+            }
+        }
+
+        System.out.println("\nEjercicio G: Muestra las species de animales en cada solicitud de adopción");
+        HashSet<String> especies=sist.especiesSolicitadas();
+        if(especies.size()==0){
+            System.out.println("No hay valores");
+        }
+        else {
+            for (String s : especies) {
+                System.out.println(s);
+            }
+        }
+
+        System.out.println("\nEjercicio H: Muestra el animal más recientemente adoptado en el sistema");
+        System.out.println(sist.animalMasReciente().toString());
     }
 }
