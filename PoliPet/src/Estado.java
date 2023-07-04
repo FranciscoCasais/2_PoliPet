25c01f33a47f7cbf4d9a5ed64d91e64053fcf72f
Mauro <101263125+PeanutSouth@users.noreply.github.com>
2023-07-04 08:57:54 -0300

    Cambio en el enum de estado

--- PoliPet/src/Estado.java
+++ PoliPet/src/Estado.java
@@ -1,7 +1,3 @@
 public enum Estado {
-    PENDIENTE("Pendiente"),APROBADA("Aprobada"),RECHAZADA("Rechazada");
-    private String estado;
-    private Estado(String estado) { this.estado=estado; }
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
+    PENDIENTE,APROBADA,RECHAZADA;
 }
